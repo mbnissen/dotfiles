e7ebd052fafe70a7ce6b5cffa629b0ac3438e5e1
Morten Nissen <morten@endavu.com>
2023-04-13 08:55:37 +0200

    Use priority for luasnip

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -15,11 +15,32 @@ lsp.ensure_installed({
   'terraformls'
 })
 
-require("luasnip.loaders.from_snipmate").lazy_load()
-
 -- (Optional) Configure lua language server for neovim
 lsp.nvim_workspace()
 
 lsp.setup()
+
+require("luasnip.loaders.from_snipmate").lazy_load()
+
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = 'luasnip' },
+    {name = 'copilot'},
+    {name = 'nvim_lsp'},
+  },
+  mapping = {
+    -- `Enter` key to confirm completion
+    -- ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    -- ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    --['<C-f>'] = cmp.action.luasnip_jump_forward(),
+    --['<C-b>'] = cmp.action.luasnip_jump_backward(),
+  }
+})
 
 
