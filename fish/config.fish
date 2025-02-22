745e3d410fc60ece875b99db1d99b97ee51763db
mbnissen <morten@endavu.com>
2025-02-22 15:21:27 +0100

    Fix asdf

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,18 @@ end
 alias lg "lazygit"
 alias vimdiff "nvim -d"
 
-source /opt/homebrew/opt/asdf/share/fish/vendor_completions.d/asdf.fish
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 direnv hook fish | source
