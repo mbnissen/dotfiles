89ab86c91ff52b5b79245f1411d136e0991806b0
mbnissen <morten@endavu.com>
2025-04-26 09:06:24 +0200

    Fix elixir shortcuts

--- nvim/lsp/elixirls.lua
+++ nvim/lsp/elixirls.lua
@@ -3,6 +3,12 @@ return {
   filetypes = { "elixir", "eelixir", "heex", "surface" },
   root_markers = { '.formatter.exs', 'mix.exs' },
   on_attach = function(client, bufnr)
+    -- Set up key mappings
+    vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Show References" })
+
+    -- Format on save
     if client.server_capabilities.documentFormattingProvider then
       vim.api.nvim_create_autocmd("BufWritePre", {
         buffer = bufnr,
