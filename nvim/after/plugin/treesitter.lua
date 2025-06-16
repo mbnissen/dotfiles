5e428df57aff0ade60678d4d8d5c7ce723322b9c
mbnissen <morten@endavu.com>
2025-06-16 10:27:50 +0200

    Update treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   -- ensure_installed = { "c", "lua", "vim", "help", "query", "javascript", "typescript", "elixir", "heex", "markdown" },
-  ensure_installed = { "c", "lua", "vim", "elixir" };
+  ensure_installed = { "c", "lua", "vim", "elixir", "typescript", "javascript" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -17,5 +17,13 @@ require 'nvim-treesitter.configs'.setup {
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
   },
 }
